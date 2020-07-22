e47025df2315ea6e6a5671b2db6010e033a325ad
Crowdbotics <team@crowdbotics.com>
2020-07-22 12:21:36 +0000

    Update store

--- src/store/services.js
+++ src/store/services.js
@@ -1,2 +1,91 @@
 import axios from "axios"
-export const apiService = {}
+const tonyapp11API = axios.create({
+  baseURL: "https://tony-app-11-dev-7815.herokuapp.com/",
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
+function api_v1_customtext_list() {
+  return tonyapp11API.get(`/api/v1/customtext/`)
+}
+function api_v1_customtext_read() {
+  return tonyapp11API.get(`/api/v1/customtext/{id}/`)
+}
+function api_v1_customtext_update(requestBody) {
+  return tonyapp11API.put(`/api/v1/customtext/{id}/`, requestBody)
+}
+function api_v1_customtext_partial_update(requestBody) {
+  return tonyapp11API.patch(`/api/v1/customtext/{id}/`, requestBody)
+}
+function api_v1_homepage_list() {
+  return tonyapp11API.get(`/api/v1/homepage/`)
+}
+function api_v1_homepage_read() {
+  return tonyapp11API.get(`/api/v1/homepage/{id}/`)
+}
+function api_v1_homepage_update(requestBody) {
+  return tonyapp11API.put(`/api/v1/homepage/{id}/`, requestBody)
+}
+function api_v1_homepage_partial_update(requestBody) {
+  return tonyapp11API.patch(`/api/v1/homepage/{id}/`, requestBody)
+}
+function api_v1_login_create() {
+  return tonyapp11API.post(`/api/v1/login/`)
+}
+function api_v1_signup_create(requestBody) {
+  return tonyapp11API.post(`/api/v1/signup/`, requestBody)
+}
+function rest_auth_login_create(requestBody) {
+  return tonyapp11API.post(`/rest-auth/login/`, requestBody)
+}
+function rest_auth_logout_list() {
+  return tonyapp11API.get(`/rest-auth/logout/`)
+}
+function rest_auth_logout_create() {
+  return tonyapp11API.post(`/rest-auth/logout/`)
+}
+function rest_auth_password_change_create(requestBody) {
+  return tonyapp11API.post(`/rest-auth/password/change/`, requestBody)
+}
+function rest_auth_password_reset_create(requestBody) {
+  return tonyapp11API.post(`/rest-auth/password/reset/`, requestBody)
+}
+function rest_auth_password_reset_confirm_create(requestBody) {
+  return tonyapp11API.post(`/rest-auth/password/reset/confirm/`, requestBody)
+}
+function rest_auth_registration_create(requestBody) {
+  return tonyapp11API.post(`/rest-auth/registration/`, requestBody)
+}
+function rest_auth_registration_verify_email_create(requestBody) {
+  return tonyapp11API.post(`/rest-auth/registration/verify-email/`, requestBody)
+}
+function rest_auth_user_read() {
+  return tonyapp11API.get(`/rest-auth/user/`)
+}
+function rest_auth_user_update(requestBody) {
+  return tonyapp11API.put(`/rest-auth/user/`, requestBody)
+}
+function rest_auth_user_partial_update(requestBody) {
+  return tonyapp11API.patch(`/rest-auth/user/`, requestBody)
+}
+export const apiService = {
+  api_v1_customtext_list,
+  api_v1_customtext_read,
+  api_v1_customtext_update,
+  api_v1_customtext_partial_update,
+  api_v1_homepage_list,
+  api_v1_homepage_read,
+  api_v1_homepage_update,
+  api_v1_homepage_partial_update,
+  api_v1_login_create,
+  api_v1_signup_create,
+  rest_auth_login_create,
+  rest_auth_logout_list,
+  rest_auth_logout_create,
+  rest_auth_password_change_create,
+  rest_auth_password_reset_create,
+  rest_auth_password_reset_confirm_create,
+  rest_auth_registration_create,
+  rest_auth_registration_verify_email_create,
+  rest_auth_user_read,
+  rest_auth_user_update,
+  rest_auth_user_partial_update
+}
